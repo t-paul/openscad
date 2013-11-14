3b9b7e67e122b9c4ebe7734c0a3c4ab5e3978792
Brody Kenrick <user.fake@server.userfake>
2013-11-14 13:37:28 +1100

    Temp workaround for #514 - Compile OpenSCAD and CGAL with gcc/g++ 4.7 (if 4.8 found)

--- scripts/uni-build-dependencies.sh
+++ scripts/uni-build-dependencies.sh
@@ -338,12 +338,25 @@ build_cgal()
     CGAL_BUILDTYPE="Debug"
   fi
 
+  #Don't use gcc 4.8 it will cause a crash :( -- is it only on linux?
+  CC=gcc
+  CXX=g++
+  if [ "`$CC --version|grep 4.8`" ]; then
+      echo "WARNING:gcc 4.8 causes a seg fault in CGAL -- see https://github.com/openscad/openscad/issues/514"
+      if [ "`gcc-4.7 --version|grep 4.7`" ]; then
+           echo "INFO:gcc 4.7 is installed -- let's use that instead (just for building CGAL and OpenSCAD)"
+           CC=gcc-4.7
+           CXX=g++-4.7
+      fi
+  fi
+  echo "CC=$CC  CXX=$CXX"
+
   DEBUGBOOSTFIND=0 # for debugging FindBoost.cmake (not for debugging boost)
   Boost_NO_SYSTEM_PATHS=1
   if [ "`echo $2 | grep use-sys-libs`" ]; then
-    cmake -DCMAKE_INSTALL_PREFIX=$DEPLOYDIR -DWITH_CGAL_Qt3=OFF -DWITH_CGAL_Qt4=OFF -DWITH_CGAL_ImageIO=OFF -DCMAKE_BUILD_TYPE=$CGAL_BUILDTYPE -DBoost_DEBUG=$DEBUGBOOSTFIND ..
+    CC=$CC CXX=$CXX   cmake -DCMAKE_INSTALL_PREFIX=$DEPLOYDIR -DWITH_CGAL_Qt3=OFF -DWITH_CGAL_Qt4=OFF -DWITH_CGAL_ImageIO=OFF -DCMAKE_BUILD_TYPE=$CGAL_BUILDTYPE -DBoost_DEBUG=$DEBUGBOOSTFIND ..
   else
-    cmake -DCMAKE_INSTALL_PREFIX=$DEPLOYDIR -DGMP_INCLUDE_DIR=$DEPLOYDIR/include -DGMP_LIBRARIES=$DEPLOYDIR/lib/libgmp.so -DGMPXX_LIBRARIES=$DEPLOYDIR/lib/libgmpxx.so -DGMPXX_INCLUDE_DIR=$DEPLOYDIR/include -DMPFR_INCLUDE_DIR=$DEPLOYDIR/include -DMPFR_LIBRARIES=$DEPLOYDIR/lib/libmpfr.so -DWITH_CGAL_Qt3=OFF -DWITH_CGAL_Qt4=OFF -DWITH_CGAL_ImageIO=OFF -DBOOST_LIBRARYDIR=$DEPLOYDIR/lib -DBOOST_INCLUDEDIR=$DEPLOYDIR/include -DCMAKE_BUILD_TYPE=$CGAL_BUILD_TYPE -DBoost_DEBUG=$DEBUGBOOSTFIND -DBoost_NO_SYSTEM_PATHS=1 ..
+    CC=$CC CXX=$CXX   cmake -DCMAKE_INSTALL_PREFIX=$DEPLOYDIR -DGMP_INCLUDE_DIR=$DEPLOYDIR/include -DGMP_LIBRARIES=$DEPLOYDIR/lib/libgmp.so -DGMPXX_LIBRARIES=$DEPLOYDIR/lib/libgmpxx.so -DGMPXX_INCLUDE_DIR=$DEPLOYDIR/include -DMPFR_INCLUDE_DIR=$DEPLOYDIR/include -DMPFR_LIBRARIES=$DEPLOYDIR/lib/libmpfr.so -DWITH_CGAL_Qt3=OFF -DWITH_CGAL_Qt4=OFF -DWITH_CGAL_ImageIO=OFF -DBOOST_LIBRARYDIR=$DEPLOYDIR/lib -DBOOST_INCLUDEDIR=$DEPLOYDIR/include -DCMAKE_BUILD_TYPE=$CGAL_BUILD_TYPE -DBoost_DEBUG=$DEBUGBOOSTFIND -DBoost_NO_SYSTEM_PATHS=1 ..
   fi
   make -j$NUMCPU
   make install
@@ -613,7 +626,8 @@ build_glew 1.9.0
 build_opencsg 1.3.2
 build_freetype 2.5.0.1
 build_fontconfig 2.11.0
-build_ragel 6.8
-build_harfbuzz 0.9.23 --with-glib=yes
+#BK - these aren't happy just yet
+#build_ragel 6.8
+#build_harfbuzz 0.9.23 --with-glib=yes
 
 echo "OpenSCAD dependencies built and installed to " $BASEDIR
