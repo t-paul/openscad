390264b0950fe6afcd0d960ffd3ecc3a75b8076f
Torsten Paul <Torsten.Paul@gmx.de>
2013-12-01 16:55:21 +0100

    Handle child nodes and update shape transformation to handle most path.
    There are still some cases that do not produce the intended output, e.g.
    a circle around the x axis flips the shape when crossing the x/y plane.

--- src/PolySetCGALEvaluator_LoftNode.cc
+++ src/PolySetCGALEvaluator_LoftNode.cc
@@ -6,12 +6,14 @@
 
 #include "module.h"
 #include "polyset.h"
+#include "dxfdata.h"
 #include "loftnode.h"
 #include "PolySetCGALEvaluator.h"
 #include "CGALEvaluator.h"
 
 #include "printutils.h"
-#include "openscad.h" // get_fragments_from_r()
+#include "openscad.h"
+#include "dxfdata.h" // get_fragments_from_r()
 
 using namespace Eigen;
 
@@ -49,14 +51,6 @@ static void add_poly(PolySet *ps,
 	ps->append_vertex(x3, y3, z3);
 	ps->append_vertex(x4, y4, z4);
 	ps->append_vertex(x1, y1, z1);
-}
-
-static double x(double x1, double y1, double sx1, double ox1, double r1) {
-	return (cos(r1) * x1 - sin(r1) * y1) * sx1 + ox1;
-}
-
-static double y(double x1, double y1, double sy1, double oy1, double r1) {
-	return (sin(r1) * x1 + cos(r1) * y1) * sy1 + oy1;
 }
 
 typedef std::vector<Vector3d> path_t;
@@ -104,17 +98,41 @@ static loft_t create_slices(path_t shape, const LoftNode &node) {
 		}
 	}	
 
-	Vector3d norm_old(0, 0, 1);
-	for (int a = 0;a < max_idx;a++) {
-		Vector3d norm = (path[a + 1] - path[a]).normalized();
-		Vector3d axis = Vector3d(0, 0, 1).cross(norm);
-		double angle = acos(Vector3d(0, 0, 1).dot(norm));
-		Transform3d rotate(AngleAxisd(angle, axis));
+	for (int a = 0; a < max_idx; a++) {
+		Vector3d normPath = (path[a + 1] - path[a]).normalized();
+
+		// Rotate the shape from X/Y plane to X/Z
+		Transform3d rotate0(AngleAxisd(M_PI / 2.0, Vector3d(1, 0, 0)));
+		
+		// Rotate the shape around the Z axis so that the norm
+		// vector of the shape points into the same direction
+		// as the path tangent vector in the X/Y plane.
+		Vector3d normXY = Vector3d(normPath.x(), normPath.y(), 0).normalized();
+		double angle = acos(Vector3d(0, 1, 0).dot(normXY));
+		if (Vector3d(1, 0, 0).dot(normXY) >= 0) {
+			angle = 2 * M_PI - angle;
+		}
+		Transform3d rotate1(AngleAxisd(angle, Vector3d(0, 0, 1)));
+
+		// Rotate the shape around the axis in the X/Y plane. The
+		// axis is perpendicular to the tangent vector of the path
+		// projected to the X/Y plane.
+		Vector3d normXYRot90(Transform3d(AngleAxisd(M_PI / 2.0, Vector3d(0, 0, 1))) * normXY);
+		double angleZ = acos(normXY.dot(normPath));
+		if (Vector3d(0, 0, 1).dot(normPath) >= 0) {
+			angleZ = 2 * M_PI - angleZ;
+		}
+		Transform3d rotate2(AngleAxisd(angleZ, normXYRot90));
+		
 		Transform3d translate(Translation3d(path[a]));
-		loft.push_back(apply_transform(apply_transform(shape, rotate), translate));
-		norm_old = norm;
+		path_t tmp(shape);
+		tmp = apply_transform(tmp, rotate0);
+		tmp = apply_transform(tmp, rotate1);
+		tmp = apply_transform(tmp, rotate2);
+		tmp = apply_transform(tmp, translate);
+		loft.push_back(tmp);
 	}
-	return loft;
+	return loft;	
 }
 
 static loft_t apply_transform(loft_t loft, const LoftNode &node)
@@ -130,58 +148,84 @@ static loft_t apply_transform(loft_t loft, const LoftNode &node)
 	return result;
 }
 
+static DxfData *evaluateChildNodes(CGALEvaluator *ev, const LoftNode &node)
+{
+	// Union all (2D) children nodes to a single DxfData
+	CGAL_Nef_polyhedron sum;
+
+	BOOST_FOREACH(AbstractNode * v, node.getChildren())
+	{
+		if (v->modinst->isBackground()) continue;
+		CGAL_Nef_polyhedron N = ev->evaluateCGALMesh(*v);
+		if (!N.isNull()) {
+			if (N.dim != 2) {
+				PRINT("ERROR: linear_extrude() is not defined for 3D child objects!");
+			} else {
+				if (sum.isNull()) sum = N.copy();
+				else sum += N;
+			}
+		}
+	}
+
+	if (sum.isNull()) return NULL;
+	DxfData *dxf = sum.convertToDxfData();
+	return dxf;
+}
+
 PolySet *PolySetCGALEvaluator::evaluatePolySet(const LoftNode &node)
 {
 	PolySet *ps = new PolySet();
+	
+	DxfData *dxf = evaluateChildNodes(&this->cgalevaluator, node);
+	if ((dxf == NULL) || dxf->paths.empty()) {
+		return NULL;
+	}
 
-	double x1 = -10, x2 = 10, y1 = -10, y2 = 10;
 	path_t shape;
-	shape.push_back(Vector3d(x1, y1, 0));
-	shape.push_back(Vector3d(x1, y2, 0));
-	shape.push_back(Vector3d(x2, y2, 0));
-	shape.push_back(Vector3d(x2, y1, 0));
+	DxfData::Path p = dxf->paths[0];
+	for (std::vector<int>::iterator it = p.indices.begin();it != p.indices.end();it++) {
+		int idx = *it;
+		Vector2d v = dxf->points[idx];
+		shape.push_back(Vector3d(v.x(), v.y(), 0));
+	}
 	
 	loft_t loft = apply_transform(create_slices(shape, node), node);
 
 	path_t bottom = loft[0];
-        add_poly(ps,
-		bottom[0].x(), bottom[0].y(), bottom[0].z(),
-		bottom[1].x(), bottom[1].y(), bottom[1].z(),
-		bottom[2].x(), bottom[2].y(), bottom[2].z(),
-		bottom[3].x(), bottom[3].y(), bottom[3].z());
+	ps->append_poly();
+	int idx = 0;
+	for (path_t::iterator it = bottom.begin();it != bottom.end();it++) {
+		Vector3d v = *it;
+		ps->append_vertex(v.x(), v.y(), v.z());
+	}
+
 	path_t top = loft[loft.size() - 1];
-	add_poly(ps,
-		top[3].x(), top[3].y(), top[3].z(),
-		top[2].x(), top[2].y(), top[2].z(),
-		top[1].x(), top[1].y(), top[1].z(),
-		top[0].x(), top[0].y(), top[0].z());
+	ps->append_poly();
+	for (path_t::reverse_iterator it = top.rbegin();it != top.rend();it++) {
+		Vector3d v = *it;
+		ps->append_vertex(v.x(), v.y(), v.z());
+	}
+
 	for (int a = 1;a < node.max_idx;a++) {
 		path_t p1 = loft[a - 1];
 		path_t p2 = loft[a];
-
-                add_poly(ps,
-			p1[0].x(), p1[0].y(), p1[0].z(),
-			p1[3].x(), p1[3].y(), p1[3].z(),
-			p2[3].x(), p2[3].y(), p2[3].z(),
-			p2[0].x(), p2[0].y(), p2[0].z());
-
-                add_poly(ps,
-			p1[3].x(), p1[3].y(), p1[3].z(),
-			p1[2].x(), p1[2].y(), p1[2].z(),
-			p2[2].x(), p2[2].y(), p2[2].z(),
-			p2[3].x(), p2[3].y(), p2[3].z());
 		
-                add_poly(ps,
-			p1[2].x(), p1[2].y(), p1[2].z(),
-			p1[1].x(), p1[1].y(), p1[1].z(),
-			p2[1].x(), p2[1].y(), p2[1].z(),
-			p2[2].x(), p2[2].y(), p2[2].z());
-		
-                add_poly(ps,
-			p1[1].x(), p1[1].y(), p1[1].z(),
-			p1[0].x(), p1[0].y(), p1[0].z(),
-			p2[0].x(), p2[0].y(), p2[0].z(),
-			p2[1].x(), p2[1].y(), p2[1].z());
+#if 0
+		ps->append_poly();
+		for (path_t::iterator it = p2.begin();it != p2.end();it++) {
+			Vector3d v = *it;
+			ps->append_vertex(v.x(), v.y(), v.z());
+		}
+#else
+		for (int idx1 = 0;idx1 < p1.size();idx1++) {
+			int idx2 = (idx1 + p1.size() - 1) % p1.size();
+			add_poly(ps,
+				p1[idx1].x(), p1[idx1].y(), p1[idx1].z(),
+				p1[idx2].x(), p1[idx2].y(), p1[idx2].z(),
+				p2[idx2].x(), p2[idx2].y(), p2[idx2].z(),
+				p2[idx1].x(), p2[idx1].y(), p2[idx1].z());
+		}
+#endif
 	}
 	
 	return ps;
